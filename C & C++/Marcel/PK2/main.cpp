b2ac54844439367f36baea250a3b4390a8634d52
Z0mbiel0ne <schlotz23marcel@gmail.com>
2017-07-23 18:40:43 +0200

    8

--- C & C++/Marcel/PK2/main.cpp
+++ C & C++/Marcel/PK2/main.cpp
@@ -7,19 +7,74 @@
 /* 
  * File:   main.cpp
  * Author: Marcel
- *
+ *+
  * Created on 22. Juli 2017, 13:20
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv) {
 
-    return 0;
+class Medium
+{
+    private:
+        const int id;
+        string titel;
+        int jahr;
+    
+    public:
+        Medium(string titel, int jahr) : id{}, titel{titel}, jahr{jahr}{};
+        int alter(){ return 2016-jahr;};
+        void virtual druckeDaten() = 0;
+        virtual ~Medium(){};
+        int getID(){return id;}
+        string getTitel(){return titel;};
+        int getJahr(){return jahr;};
+
+};
+
+class Audio : public Medium
+{
+    public: 
+        Audio(string titel, int jahr) : Medium(titel, jahr){}
+        void druckeDaten(){
+            cout << "ID: " << getID() << " Titel: " << getTitel() << " Jahr: "<< getJahr() << endl;  
+        }
+        virtual ~Audio() {};
+
+};
+
+class Bild : public Medium
+{
+    private:
+        string ort;
+    public:
+        Bild(string titel, int jahr, string ort): Medium(titel, jahr), ort{ort}{}
+        string getOrt(){return ort;}
+        void druckeDaten(){
+            cout << "ID: " << getID() << " Titel: " << getTitel() << " Jahr: "<< getJahr() << " Ort: " << getOrt();  
+        }
+        virtual ~Bild() {}
+
+};
+
+int main(int argc, char** argv) 
+{
+    Medium *test[2];
+    
+    Audio *a;
+    a = new Audio("Test", 1997);
+    
+    Bild *b;
+    b = new Bild("Test", 1997, "Essen");
+    
+    test[0] = a;
+    test[1] = b;
+    test[0]->druckeDaten();
+    test[1]->druckeDaten();
 }
-
