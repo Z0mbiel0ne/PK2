8d1af19bde1bf6bdea6422705c59eacc77596da1
exashox <exashox@gmail.com>
2017-07-24 12:25:20 +0200

    Add Praktikum 6
    Not Finish

---
+++ C & C++/Stephan/Praktikum6/main.c
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.c
+ * Author: Klimek
+ *
+ * Created on 24. Juli 2017, 12:22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Aufgabe 1");
+    // TODO Aufgabe 1
+    printf("\n\n");
+    printf("Aufgabe 2");
+    // TODO Aufgabe 2
+    printf("\n\n");
+    printf("Aufgabe 3");
+    //TODO Aufgabe 3
+}
+
---
+++ C & C++/Stephan/Praktikum6/main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: Klimek
+ *
+ * Created on 24. Juli 2017, 12:23
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_H */
+
